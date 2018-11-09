380763c38007a56dd338836c570b49033c6cb081
Ilya <cyansmoking@gmail.com>
2018-11-09 20:20:50 +0300

    NN

--- app/src/main/java/com/sevcabel/sevcabelport/SplashActivity.kt
+++ app/src/main/java/com/sevcabel/sevcabelport/SplashActivity.kt
@@ -1,12 +1,20 @@
 package com.sevcabel.sevcabelport
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        routeToOtherActivity()
+        finish()
+    }
+    private fun routeToOtherActivity(){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
