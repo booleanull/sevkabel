462583ce69901090ab39534dcd835ab3c3972474
msvetkov <44121460+msvetkov@users.noreply.github.com>
2018-11-10 03:06:33 +0300

    Added all vk info to profile

--- app/src/main/java/com/sevcabel/sevcabelport/profile/ProfileFragment.kt
+++ app/src/main/java/com/sevcabel/sevcabelport/profile/ProfileFragment.kt
@@ -9,6 +9,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import com.google.firebase.database.*
 import com.sevcabel.sevcabelport.R
 import com.sevcabel.sevcabelport.utils.SevcabelApplication
@@ -19,6 +20,7 @@ import java.net.URL
 const val TAG:String = "ProfileFragment"
 
 class ProfileFragment : Fragment() {
+    val userID: String = SevcabelApplication.getUserId()
 
     private lateinit var database: FirebaseDatabase
     private lateinit var myRef: DatabaseReference
@@ -33,23 +35,27 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val avatarView: ImageView = avatar_image
-        setAvatar(avatarView)
+        val nameView: TextView = name_text
+        setUserInfo(avatarView, nameView)
+
     }
 
-    private fun setAvatar(avatarView: ImageView){
+    private fun setUserInfo(avatarView: ImageView, nameView: TextView){
         database = FirebaseDatabase.getInstance()
         myRef = database.reference
         myRef!!.addListenerForSingleValueEvent( object : ValueEventListener {
 
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 if (dataSnapshot.exists()) {
-                    val userID: String = SevcabelApplication.getUserId()
                     Log.d(TAG, userID)
                     val urlString = dataSnapshot.child("user").child(userID).child("photo").getValue(String::class.java)
+                    val surname = dataSnapshot.child("user").child(userID).child("surname").getValue(String::class.java)
+                    val lastname = dataSnapshot.child("user").child(userID).child("lastname").getValue(String::class.java)
                     Log.d(TAG, urlString)
                     Picasso.get()
                             .load(Uri.parse(urlString))
                             .into(avatarView)
+                    nameView.text = ("$surname $lastname")
                 }
             }
 
